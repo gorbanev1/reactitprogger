3a36c28abd5eb962e7160f123e6ae1a7046e3da0
gorban <gorbanev_i@mail.ru>
2023-12-12 20:30:57 +0300

    before User component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,44 +1,51 @@
 import React from "react";
 class Users extends React.Component{
-    users=[
-        {
-            id: 1,
-            firstname: 'Bob',
-            lastName: 'Marley',
-            bio: 'dsfsaf as ds fa sfs',
-            age: 40,
-            pidor: true,
-        },
-        {
-            id: 2,
-            firstname: 'Hui',
-            lastName: 'Pizdov',
-            bio: 'SRET SRET',
-            age: 77,
-            pidor: false,
+    constructor(props){
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Bob',
+                    lastName: 'Marley',
+                    bio: 'dsfsaf as ds fa sfs',
+                    age: 40,
+                    pidor: true,
+                },
+                {
+                    id: 2,
+                    firstname: 'Hui',
+                    lastName: 'Pizdov',
+                    bio: 'SRET SRET',
+                    age: 77,
+                    pidor: false,
+                }
+        
+            ]
         }
+        this.arr2=this.state.users.map((el)=><div className="user" key={el.id}>
+        <h3>{el.firstname} {el.lastName}</h3>
+        <p>{el.bio}</p>
+        <b>{el.pidor ? "Пидор": "Хуй"}</b>
+        </div>)
+    }
 
-    ]
-    arr2=this.users.map((el)=><div key={el.id} className="user">
-    <h3>{el.firstname} {el.lastName}</h3>
-    <p>{el.bio}</p>
-    <b>{el.pidor ? "Пидор": "Хуй"}</b>
-    </div>)
 
 
     render(){
-         console.log(this.arr2[1])
-         if (this.users.length>0)
+        //  console.log(this.arr2[1])
+         if (this.state.users.length>0)
 
      return(
        
        <div>
-        {this.arr2}
+        {/* {this.arr2} */}
         
-        {/* {const arr2=this.users.map((el)=><div>
+        {this.state.users.map((el)=><div className="user" key={el.id}>
             <h3>{el.firstname} {el.lastName}</h3>
             <p>{el.bio}</p>
-        </div>)}{ console.log(this.arr2[1])} */}
+            <b>{el.pidor ? "Пидор": "Хуй"}</b>
+        </div>)}
         </div>
        
      )
